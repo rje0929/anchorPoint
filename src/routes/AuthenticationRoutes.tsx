0aa20a999273002b99691bfe06f4aac978212f35
Robbie Edwards <edwardsrobbie@gmail.com>
2025-08-20 23:10:56 -0400

    more clean up

--- src/routes/AuthenticationRoutes.tsx
+++ src/routes/AuthenticationRoutes.tsx
@@ -6,10 +6,6 @@ import MinimalLayout from 'layout/MinimalLayout';
 
 // maintenance routing
 const MaintenanceError = Loadable(lazy(() => import('views/pages/maintenance/Error')));
-const MaintenanceError500 = Loadable(lazy(() => import('views/pages/maintenance/Error500')));
-const MaintenanceComingSoon1 = Loadable(lazy(() => import('views/pages/maintenance/ComingSoon/ComingSoon1')));
-const MaintenanceComingSoon2 = Loadable(lazy(() => import('views/pages/maintenance/ComingSoon/ComingSoon2')));
-const MaintenanceUnderConstruction = Loadable(lazy(() => import('views/pages/maintenance/UnderConstruction')));
 
 // landing & contact-us routing
 const PagesLanding = Loadable(lazy(() => import('views/pages/landing')));
@@ -24,23 +20,6 @@ const AuthenticationRoutes = {
       path: '/pages/error',
       element: <MaintenanceError />
     },
-    {
-      path: '/pages/500',
-      element: <MaintenanceError500 />
-    },
-    {
-      path: '/pages/coming-soon1',
-      element: <MaintenanceComingSoon1 />
-    },
-    {
-      path: '/pages/coming-soon2',
-      element: <MaintenanceComingSoon2 />
-    },
-    {
-      path: '/pages/under-construction',
-      element: <MaintenanceUnderConstruction />
-    },
-
     {
       path: '/pages/landing',
       element: <PagesLanding />
